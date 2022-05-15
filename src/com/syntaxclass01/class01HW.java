1dde06e1e03a8abf4c0b478cb9a894ae59d04f86
eduardito2009 <cm655176@gmail.com>
2022-05-15 07:43:44 -0400

    1st commit

---
+++ src/com/syntaxclass01/class01HW.java
@@ -0,0 +1,11 @@
+package com.syntaxclass01;
+
+public class class01HW {
+    public static void main(String[] args) {
+        int n = 100;
+        System.out.println("Numbers from 1 to "+n+" are: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
