791c967c744cabd8955a9cac89f713bbdf9c41bd
eduardito2009 <cm655176@gmail.com>
2022-05-15 09:25:30 -0400

    new update

--- src/com/syntaxclass01/class01HW.java
+++ src/com/syntaxclass01/class01HW.java
@@ -2,15 +2,11 @@ package com.syntaxclass01;
 
 public class class01HW {
     public static void main(String[] args) {
-        int n = 100;
+        int n = 50;
         System.out.println("Numbers from 1 to "+n+" are: ");
         for (int i = 1; i <= n; i++) {
             System.out.println(i + " ");
-        }
-        int n1 = 50;
-        System.out.println("Numbers from 1 to "+n1+" are: ");
-        for (int i = 1; i <= n1; i++) {
-            System.out.println(i + " ");
+
         }
 
     }
